d113f9b45fe49dc4ad6d37b02eeee1886f765edd
João Nunes <115576458+joaonunesss@users.noreply.github.com>
2023-11-24 12:35:55 +0000

    Add files via upload

---
+++ microshell.c
@@ -0,0 +1,63 @@
+#include "microshell.h"
+
+int ft_perror(char *str)
+{
+    while(*str)
+        write(2, str++, 1);
+    return (1);
+}
+
+int ft_cd(char **av, int i)
+{
+    if (i != 2)
+        return (ft_perror(BAD));
+    else if (chdir(av[1]) == -1)
+            return (ft_perror(PATH), ft_perror(av[1]), ft_perror("\n"));
+    return (0);
+}
+
+int ft_exec(char **av, char **env, int i)
+{
+    int fd[2];
+    int g_exit;
+    int has_pipe = av[i] && !strcmp(av[i], "|");
+    int pid;
+
+    if (has_pipe && pipe(fd) == -1)
+        return (ft_perror(FATAL));
+    pid = fork();
+    if (pid == 0)
+    {
+        av[i] = NULL;
+        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+            return (ft_perror(FATAL));
+        execve(*av, av, env);
+        return (ft_perror(EXEC), ft_perror(*av), ft_perror("\n"));
+    }
+    waitpid(pid, &g_exit, 0);
+    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+        return (ft_perror(FATAL));
+    return (WEXITSTATUS(g_exit));
+
+}
+
+int main(int ac, char **av, char **env)
+{
+    int i = 0;
+    int g_exit = 0;
+
+    if(ac < 2)
+        return (1);
+    while(av[i] && av[++i])
+    {
+        av += i;
+        i = 0;
+        while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+            i++;
+        if (!strcmp(*av, "cd"))
+            g_exit = ft_cd(av, i);
+        else if (i)
+            g_exit = ft_exec(av, env, i);
+    }
+    return (g_exit);
+}
---
+++ microshell.h
@@ -0,0 +1,14 @@
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+# define BAD "error: cd: bad arguments\n"
+# define PATH "error: cd: cannot change directory to "
+# define FATAL "error: fatal\n"
+# define EXEC "error: cannot execute "
+
+#endif
